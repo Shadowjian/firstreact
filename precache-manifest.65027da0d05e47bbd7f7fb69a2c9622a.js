247f134f62cf7eb689713899e779de6030b14b81
Shadowjian <karakalel.adorable@gmail.com>
2022-11-19 22:06:10 +0800

    Updates

---
+++ precache-manifest.65027da0d05e47bbd7f7fb69a2c9622a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd115d6e0969c651e8e9",
+    "url": "/firstreact/static/js/main.a03c8ad4.chunk.js"
+  },
+  {
+    "revision": "123392da67e38e09f938",
+    "url": "/firstreact/static/js/runtime~main.a7d87b6e.js"
+  },
+  {
+    "revision": "209070e5cca4fd66edba",
+    "url": "/firstreact/static/js/2.1d535f4e.chunk.js"
+  },
+  {
+    "revision": "c17260d27383af76bc19219ee0c98f50",
+    "url": "/firstreact/index.html"
+  }
+];
